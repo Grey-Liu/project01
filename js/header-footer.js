c207e21e94fc7f05a38b6c671f7be4a43d483c8c
Grey-Liu <sugarfree5807@gmail.com>
2025-06-18 22:38:39 +0800

    update

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,11 +1,15 @@
-        //開啟menu
-        const menuBtn = document.querySelector('.menu-button');
-        const sidebar = document.getElementById('sidebar');
+const menuBtn  = document.querySelector('.menu-button');
+const sidebar  = document.getElementById('sidebar');
+const overlay  = document.getElementById('sidebarOverlay'); 
 
-        menuBtn.addEventListener('click', () => {
-            sidebar.classList.toggle('open');
-        });
-        //置頂
-        document.querySelector('.btn-top').addEventListener('click', () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-        });
+menuBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('open');
+    overlay.classList.toggle('active');
+    menuBtn.classList.toggle('active');         
+});
+
+overlay.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    overlay.classList.remove('active');
+    menuBtn.classList.remove('active');         
+});
